3decba7a5f2dc303878df67f08dc265a871e435b
Rehan Bhargava <137583838+johnCode07@users.noreply.github.com>
2025-09-15 11:40:58 -0400

    Update project-info.js

--- project-info.js
+++ project-info.js
@@ -4,13 +4,13 @@ export const projects = [
     title: "Robotics Turret Design",
     desc: "Prototype Turret design for 2026 FRC game",
     img: "Turretv1.png",
-    link: "https://cad.onshape.com/..."
+    link: "https://cad.onshape.com/documents/dad9a5143e948fa790757cbd/w/f9912f3b918c5744adae4bed/e/58a58bbf9e93fa3f64d450c1?renderMode=0&uiState=68c833703e33063612d30182"
   },
   {
     title: "FRC Reefscape Offseason Bot",
     desc: "Concept robot for the 2025 FRC game",
-    img: "",
-    link: "https://github.com/rehan/portfolio-website"
+    img: "OffseasonBot.png",
+    link: "https://cad.onshape.com/documents/66549edfba56524834dc1d6f/w/15077c65e03286e083abeeae/e/7aabf745f93416db886b7b8a?renderMode=0&uiState=68c8333d10ef7f9435a7fb20"
   },
   {
     title: "poop",
@@ -22,12 +22,12 @@ export const projects = [
     title: "Terrabot",
     desc: "Programmed self sustaining gardening robot that takes data from sensors to optimize growth conditions",
     img: "",
-    link: "https://github.com/rehan/portfolio-website"
+    link: "awaiting"
   },
   {
     title: "Frc Reefscape Elevator",
     desc: "Designed and fabricated 5895 Jormungandr 2 stage belt driven elevator, which went on to win our division at worlds",
-    img: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=400&q=80",
-    link: "https://github.com/rehan/3d-desk-organizer"
+    img: "ElevatorPng.png",
+    link: "https://cad.onshape.com/documents/ed27b56935eded6d35c01e6f/w/4cc3593340dbb2453637039c/e/139f5e426054fd58c0a2554b?renderMode=0&uiState=68c832ce3e52696d53eb3dd6"
   },
 ];
